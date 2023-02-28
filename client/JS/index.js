ec389aa570d1fe1669fac3f11c5e77e589842ef6
codegram <omondihills02@gmail.com>
2023-02-28 14:20:26 +0300

    added event listeners to the buttons

--- client/JS/index.js
+++ client/JS/index.js
@@ -1,2 +1,10 @@
 let open_btn=document.querySelector("#open_nav");
 let close_btn=document.querySelector("#close_nav");
+
+
+
+open_btn.addEventListener('click', openMenu);
+close_btn.addEventListener('click', closeMenu);
+
+
+

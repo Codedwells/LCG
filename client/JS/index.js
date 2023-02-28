7f63d475d4faa1c31f1fe4cf5d63b963bd94a611
codegram <omondihills02@gmail.com>
2023-02-28 14:25:09 +0300

    added functionality to the nav buttons when clicked

--- client/JS/index.js
+++ client/JS/index.js
@@ -7,4 +7,12 @@ open_btn.addEventListener('click', openMenu);
 close_btn.addEventListener('click', closeMenu);
 
 
+function openMenu(){
+    open_btn.style.display="none";
+    close_btn.style.display="block";
+}
 
+function closeMenu(){
+    open_btn.style.display="block";
+    close_btn.style.display="none";
+}

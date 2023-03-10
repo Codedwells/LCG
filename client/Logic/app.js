a9a56f97ad26f636fc23f17f152daf3107fb730b
codegram <omondihills02@gmail.com>
2023-03-10 14:08:20 +0300

    added two buttons to toggle on and off the nav menu

--- client/Logic/app.js
+++ client/Logic/app.js
@@ -1,1 +1,20 @@
 console.log('hello world!!')
+
+let open_btn=document.querySelector("#open_nav");
+let close_btn=document.querySelector("#close_nav");
+
+
+
+open_btn.addEventListener('click', openMenu);
+close_btn.addEventListener('click', closeMenu);
+
+
+function openMenu(){
+    open_btn.style.display="none";
+    close_btn.style.display="block";
+}
+
+function closeMenu(){
+    open_btn.style.display="block";
+    close_btn.style.display="none";
+}

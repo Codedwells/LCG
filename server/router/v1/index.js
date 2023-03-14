a269af3872a8449e6bd97334cf6d75af9fa819c1
coddedwells <wells.mail.13@gmail.com>
2023-03-14 14:54:38 +0300

    👌 IMPROVE: set for preview

--- server/router/v1/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-const test = require('./test')
-
-router.use('/test',test)
-
-module.exports = router
--- server/router/v1/test.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    res.send(`<h1>Hello world!!</h1>`);
-    res.end()
-});
-
-module.exports = router
